ed3d071fe393ce966f4cdb0d6c06d33a21739d67
Vardas Pavarde <el@pastas.com>
2022-07-11 10:18:10 +0300

    models, admin added

--- postit/postit/settings.py
+++ postit/postit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-li7mre)d&!*d-8jwja+#frhp!6b!^!xy(4+kcfeo$7eflk1vai'
+SECRET_KEY = 'local_setting.SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
